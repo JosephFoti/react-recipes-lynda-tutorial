aba0eb28cea4892b3335a45cfd93fb0ad6150c3c
Joseph Foti <eljofo@gmail.com>
2018-07-20 15:33:54 -0400

    404 fallback page added

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Favorites from './Favorites';
 import Header from './Header';
+import NotFound from './NotFound';
 
 const App = () => (
   <BrowserRouter>
     <main>
       <Header />
-      <Route exact path="/" component={Home} />
-      <Route path="/favorites" component={Favorites} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/favorites" component={Favorites} />
+        <Route component={NotFound} />
+      </Switch>
     </main>
   </BrowserRouter>
 );
---
+++ src/components/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NotFound = () => <h1 className="px4">404 Error: Dish not found!</h1>;
+
+export default NotFound;
